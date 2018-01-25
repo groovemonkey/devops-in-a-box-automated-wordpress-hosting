d6e4363ff1a9be1cf9b5d89bf0dbe8fca02cb6f3
Dave Cohen <dave@project768.com>
2018-01-24 19:33:00 -0800

    move instance modification functions into fabric

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -8,14 +8,79 @@ import googleapiclient
 
 
 @task
-def create_instance():
-    """Spin up compute instance from base image."""
+def create_instance(project, zone, name, machinetype="n1-standard-1", source_disk_image="", metadata={}):
+    """
+    Create a compute instance in the supplied project/zone, using the supplied
+    machine type
+    disk image
+    metadata
+
+    # Sample metadata dict
+    metadata = {
+        'items': [{
+            # Startup script is automatically executed by the
+            # instance upon startup.
+            'key': 'startup-script',
+            'value': startup_script
+        }]
+    }
+    """
     env.run = lrun
     env.hosts = ['localhost']
 
     # Initialize
     compute = googleapiclient.discovery.build('compute', 'v1')
 
+    # Configure the machine
+    machine_type = "zones/{}/machineTypes/{}".format(zone, machinetype)
+
+    # TODO: are we doing this here?
+    startup_script = open(
+        os.path.join(
+            os.path.dirname(__file__), 'startup-script.sh'), 'r').read()
+
+    config = {
+        'name': name,
+        'machineType': machine_type,
+
+        # Specify the boot disk and the image to use as a source.
+        'disks': [
+            {
+                'boot': True,
+                'autoDelete': True,
+                'initializeParams': {
+                    'sourceImage': source_disk_image,
+                }
+            }
+        ],
+
+        # Specify a network interface with NAT to access the public
+        # internet.
+        'networkInterfaces': [{
+            'network': 'global/networks/default',
+            'accessConfigs': [
+                {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+            ]
+        }],
+
+        # Allow the instance to access cloud storage and logging.
+        'serviceAccounts': [{
+            'email': 'default',
+            'scopes': [
+                'https://www.googleapis.com/auth/devstorage.read_write',
+                'https://www.googleapis.com/auth/logging.write'
+            ]
+        }],
+
+        # Metadata is readable from the instance and allows you to
+        # pass configuration from deployment scripts to instances.
+        'metadata': metadata
+    }
+
+    return compute.instances().insert(
+        project=project,
+        zone=zone,
+        body=config).execute()
 
 
 # @task
@@ -42,6 +107,11 @@ def create_site_user(dbname, dbuser, dbpassword):
     pass
 
 
+def list_instances(compute, project, zone):
+    result = compute.instances().list(project=project, zone=zone).execute()
+    return result['items']
+
+
 @task
 def create_wp_config():
     """Create a wp-config.php file from site metadata (hostname, mysql auth/endpoint info, etc.)."""
@@ -66,6 +136,18 @@ def backup_site_files():
     pass
 
 
+@task
+def delete_instance(project, zone, name):
+    """
+    Delete a compute instance.
+    """
+    # Initialize
+    compute = googleapiclient.discovery.build('compute', 'v1')
+
+    return compute.instances().delete(
+        project=project,
+        zone=zone,
+        instance=name).execute()
 
 
 
